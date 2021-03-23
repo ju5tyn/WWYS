eb1d3232dad5a53d1649872228db34378ee1b115
Justyn Henman <justynhenman@me.com>
2021-03-23 08:47:24 +0000

    Initial Commit

---
+++ WWYS/WWYSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWYSApp.swift
+//  WWYS
+//
+//  Created by Justyn Henman on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WWYSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
